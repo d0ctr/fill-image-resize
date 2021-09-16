b375ed8b2afc5549b460d64e73bab80175d9ec76
Danila Vasilev <33842017+d0ctr@users.noreply.github.com>
2021-09-16 17:10:49 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='fill-image-resize',
+version='0.1',
+description='Resize image without stretching (as Fill resize in Figma)',
+author='d0ctr',
+author_email='mail@vasilev.ml',
+license='MIT',
+packages=['fill-image-resize'],
+zip_safe=False)
