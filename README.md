b45171b47f776eb9dde09e7f3b0eb4b7504b337a
Danila Vasilev <mail@vasilev.ml>
2021-09-16 19:11:36 +0300

    Added docstring and minor changes

--- README.md
+++ README.md
@@ -3,15 +3,19 @@ This package is dedicated to solve problem of resizing images from one size and 
 
 ## What happenes
 
-
 ![fill_image_resize](https://user-images.githubusercontent.com/33842017/133638932-0c9f9053-4e32-4675-8c44-988b1efd8fab.png)
 
 ## How to use
 ```python
 from fill-image-resize import resize
 
+
 path = 'image.png'
 desired_width = 1020
 desired_height = 1000
 resized_image = resize(path, desired_width, desired_height)
 resized_image.save('resized_image.png')
+```
+
+# Requirements
+### **Pillow** >= 7.0.0
--- src/fill_image_resize/resizer.py
+++ src/fill_image_resize/resizer.py
@@ -1,34 +1,62 @@
 import math
+from typing import Type
+
 from PIL import Image
 
-def resize(path, resulting_width, resulting_height):
-  img = Image.open(path)
 
-  width_original = img.width
-  height_original = img.height
+def resize(path: str, resulting_width: int, resulting_height: int) -> Image.Image:
+    """
+    Opens and resized an image to the desired width and height
 
-  scaling_factor = math.gcd(resulting_width, resulting_height)
-  new_width_ratio = resulting_width / scaling_factor
-  new_height_ratio = resulting_height / scaling_factor
+    This function uses Pillow to open file and resize it without distring it.
+    An example of such behaviour can be found in Figma, while managing an
+    image you can select "Fill" type of transforming, thus your image
+    wouldn't be stretched when altering aspect ratio, but resized and cropped.
 
-  width_diff = width_original / new_width_ratio
-  height_diff = height_original / new_height_ratio
+    :param path: A filename (string), pathlib.Path object or a file object.
+       The file object must implement ``file.read``,
+       ``file.seek``, and ``file.tell`` methods,
+       and be opened in binary mode.
+    :param resulting_width: The resulting width of returned image. Should be 
+        an integer.
+    :param resulting_height: The resulting height of returned image. Should be 
+        an integer.
+    :returns: An :py:class:`~PIL.Image.Image` object.
+    :exception FileNotFoundError: If the file cannot be found.
+    :exception PIL.UnidentifiedImageError: If the image cannot be opened and
+       identified.
+    :exception ValueError: If the ``mode`` is not "r", or if a ``StringIO``
+       instance is used for ``fp``.
+    :exception TypeError: If ``formats`` is not ``None``, a list or a tuple.
+    """
+    
+    img = Image.open(path)
 
-  if width_diff >= height_diff:
-      ratio_multiplier = height_diff
-  else:
-      ratio_multiplier = width_diff
+    width_original = img.width
+    height_original = img.height
 
-  crop_width = ratio_multiplier * new_width_ratio
-  crop_height = ratio_multiplier * new_height_ratio
+    scaling_factor = math.gcd(resulting_width, resulting_height)
+    new_width_ratio = resulting_width / scaling_factor
+    new_height_ratio = resulting_height / scaling_factor
 
-  left = (img.width - crop_width) / 2
-  top = (img.height - crop_height) / 2
-  right = (img.width + crop_width) / 2
-  bottom = (img.height + crop_height) / 2
-  
-  img = img.crop(box=(left, top, right, bottom))
-  
-  img = img.resize((resulting_width, resulting_height))
-  
-  return img
+    width_diff = width_original / new_width_ratio
+    height_diff = height_original / new_height_ratio
+
+    if width_diff >= height_diff:
+        ratio_multiplier = height_diff
+    else:
+        ratio_multiplier = width_diff
+
+    crop_width = ratio_multiplier * new_width_ratio
+    crop_height = ratio_multiplier * new_height_ratio
+
+    left = math.floor((img.width - crop_width) / 2)
+    top = math.floor((img.height - crop_height) / 2)
+    right = math.floor((img.width + crop_width) / 2)
+    bottom = math.floor((img.height + crop_height) / 2)
+    
+    img = img.crop(box=(left, top, right, bottom))
+    
+    img = img.resize((resulting_width, resulting_height))
+    
+    return img
