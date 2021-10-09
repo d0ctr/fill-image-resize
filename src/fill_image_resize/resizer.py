486d3ad865a4358a3ff3b688fac42f4f2c261641
Danila Vasilev <mail@vasilev.ml>
2021-10-09 20:12:11 +0300

    Added getting an image from url and automatic saving

--- src/fill_image_resize/resizer.py
+++ src/fill_image_resize/resizer.py
@@ -1,31 +1,59 @@
 import math
-from typing import Type
+import requests
+import shutil
+from typing import Optional
+from io import BytesIO
 
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 
-def resize(path: str, resulting_width: int, resulting_height: int) -> Image.Image:
+class BadPathError(Exception):
+    """Base class for errors connected with provided path."""
+    
+    def __init__(self, message = 'Can not get an image by provided path'):
+        self.message = message
+        super().__init__(self.message)
+
+class BadLinkError(Exception):
+    """Exception is raised for bad image URLs"""
+
+    def __init__(self, message='Can not get an image by provided URL'):
+        self.message = message
+        super().__init__(self.message)
+
+class BadImageError(Exception):
+    """Exception is raised when can provided file is not an image"""
+    
+    def __init__(self, message='This is not an image'):
+        self.message = message
+        super().__init__(self.message)
+
+
+def resize(path: str, resulting_width: int, resulting_height: int, save: Optional[str] = None) -> Image.Image:
     """
     Opens and resizes an image to the desired width and height
 
-    This function uses Pillow to open file and resize it without distorting it.
+    This function uses Pillow to open a file or extract an image from bytes
+    and resize it without distorting it.
     An example of such behaviour can be found in Figma, while managing an
     image you can select "Fill" type of transforming, thus your image
     wouldn't be stretched when altering aspect ratio, but resized and cropped.
 
-    :param path: A filename (string), pathlib.Path object or a file object.
-        The file object must implement ``file.read``, ``file.seek``,
-        and ``file.tell`` methods, and be opened in binary mode.
-    :param resulting_width: Must be > 0. The resulting width of returned image.
+    :param path: A filename (string) or URL. If it is an URL must start with 
+        'HTTP' or 'HTTPS'.
+    :param resulting_width: Must be > 0. The resulting width of returned image. 
         Should be an integer.
     :param resulting_height: Must be > 0. The resulting height of returned image. 
         Should be an integer.
-    :returns: An :py:class:`~PIL.Image.Image` object.
-    :exception FileNotFoundError: If the file cannot be found. Or if the output
+    :param save: Optional filename (string) for saving file.
+    :returns: A tuned :py:class:`~PIL.Image.Image` object (has a field `original`
+        containing an original image of the same type).
+    :exception BadPathError: If the file cannot be found. Or if the output
         format could not be determined from the file name.  Use the format
         option to solve this.
-    :exception PIL.UnidentifiedImageError: If the image cannot be opened and
+    :exception BadImageError: If the image cannot be opened and
         identified.
+    :excpetion BadLinkError: If can not get an image from url.
     :exception ValueError: If a ``StringIO`` instance is used for ``fp``.
         Or if the output format could not be determined from the file name.
         Use the format option to solve this. Or if resulting_width or
@@ -37,10 +65,22 @@ def resize(path: str, resulting_width: int, resulting_height: int) -> Image.Imag
     if resulting_width <= 0 or resulting_height <= 0:
         raise ValueError('resulting_width and resulting_height must be higher than zero')
     
-    img = Image.open(path)
+    if path[0:4] == 'http':
+        res = requests.get(path)
+        if res.status_code == 200:
+            path = BytesIO(res.content) # this is actually bad but never mind
+        else:
+            raise BadLinkError
 
-    width_original = img.width
-    height_original = img.height
+    try:
+        img_original = Image.open(path)
+    except FileNotFoundError:
+        raise BadPathError
+    except UnidentifiedImageError:
+        raise BadImageError
+
+    width_original = img_original.width
+    height_original = img_original.height
 
     scaling_factor = math.gcd(resulting_width, resulting_height)
     new_width_ratio = resulting_width / scaling_factor
@@ -57,13 +97,17 @@ def resize(path: str, resulting_width: int, resulting_height: int) -> Image.Imag
     crop_width = ratio_multiplier * new_width_ratio
     crop_height = ratio_multiplier * new_height_ratio
 
-    left = math.floor((img.width - crop_width) / 2)
-    top = math.floor((img.height - crop_height) / 2)
-    right = math.floor((img.width + crop_width) / 2)
-    bottom = math.floor((img.height + crop_height) / 2)
+    left = math.floor((width_original - crop_width) / 2)
+    top = math.floor((height_original - crop_height) / 2)
+    right = math.floor((width_original + crop_width) / 2)
+    bottom = math.floor((height_original + crop_height) / 2)
     
-    img = img.crop(box=(left, top, right, bottom))
+    img = img_original.copy().crop(box=(left, top, right, bottom))
     
     img = img.resize((resulting_width, resulting_height))
+
+    if (save):
+        img.save(save)
+    img.original = img_original
     
     return img
